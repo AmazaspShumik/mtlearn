7d0e11eb4dc01c6fb1af5bbb098400f18961c3a6
AmazaspShumik <amazasp.shaumyan@gmail.com>
2019-12-03 22:11:33 -0800

    mixture of experts layer

---
+++ layers/moe.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from typing import List, Dict, Tuple, Union
+
+import numpy as np
+import tensorflow as tf
+from tf.keras.layers import Layer, Dense, Dropout
+
+
+
+class ExpertUtilizationDropout(Layer):
+  """
+  Helper layer for Mixture of Experts Layer, allows to drop some of the experts
+  and then renormalize other experts, so that we still have probability density
+  function for expert utilization.
+
+  Parameters
+  ----------
+  dropout_rate: float (between 0 and 1), optional (Default=0.1)
+    Probability of drop
+
+  References
+  ----------
+  [1] Recommending What Video to Watch Next: A Multitask Ranking System, 2019.
+      Zhe Zhao
+  """
+  def __init__(self,
+               dropout_rate: float=0.1,
+               **kwargs
+               ) -> None:
+    super().__init__(**kwargs)
+    self.kwargs = kwargs
+    self.dropout_rate = dropout_rate
+
+  def build(self, 
+            batch_input_shape: Tuple[int]
+            ) -> None:
+    """
+    Builds main component of the layer by creating dropout layer that has
+    the same binary mask for each element in the batch.
+
+    Parameters
+    ----------
+    batch_input_shape: tuple
+      Shape of the inputs
+    """
+    self.drop_layer = Dropout(self.dropout_rate,
+                              noise_shape=(1,*batch_input_shape[1:]),
+                              **self.kwargs
+                             )
+    super().build(batch_input_shape)
+    
+  def call(self, 
+           inputs: Union[np.array, tf.Tensor],
+           training: bool,
+           ) -> tf.Tensor:
+    """
+    Defines forward pass for the layer, by dropping experts and then 
+    renormalizing utilization probabilities for remaining experts.
+
+    Parameters
+    ----------
+    inputs: np.array or tf.Tensor
+      Inputs to the layer (can be numpy array, tf.Tensor)
+
+    training: bool
+      True if layer is called in training mode, False otherwise 
+
+    Returns
+    -------
+    : tf.Tensor
+      Renormalized probabilities assigned to each expert
+    """
+    expert_probs_drop = self.drop_layer(inputs, training)
+    return expert_probs_drop / tf.reduce_sum(expert_probs_drop,
+                                             keepdims=True,
+                                             axis=1)
+    
+    def get_config(self) -> Dict:
+      """ Config of the ExpertUtilizationDropout layer """
+      base_config = super().get_config()
+      return {**base_config, "dropout_rate": self.dropout_rate}
+
+
+class MixtureOfExpertsLayer(Layer):
+  """
+  Mixture of Experts Layer
+
+  Parameters
+  ----------
+  experts: List of Layers
+      List of experts, each expert is expected to be a instance of Layer class.
+
+  add_dropout: bool, optional (Default=False)
+      Adds dropout after softmax layer, helps to avoid collapse of the experts 
+      and their imbalanced utilization. Was used by Zhe Zhao in [1].
+
+  dropout_rate: float (between 0 and 1), optional (Default=0.1)
+      Fraction of the experts to drop. 
+
+  References
+  ----------
+  [1] Recommending What Video to Watch Next: A Multitask Ranking System, 2019.
+      Zhe Zhao
+  [2] Outrageously Large Neural Networks: The Sparsely-Gated Mixture-of-Experts
+      Layer, 2017. Noam Shazeer, Azalia Mirhoseini.
+  """
+  def __init__(self, 
+               expert_layers: List[Layer],
+               add_dropout: bool=False,
+               dropout_rate: float=0.1,
+               **kwargs
+               ) -> None:
+    super().__init__(**kwargs)
+    self.kwargs = kwargs
+    self.expert_layers = expert_layers
+    self.n_experts = len(expert_layers)
+    self.add_dropout = add_dropout
+    self.dropout_rate = dropout_rate
+
+  def build(self, 
+            batch_input_shape
+            ) -> None:
+    """
+    Builds 
+    """
+    self.expert_probs = Dense(self.n_experts,"softmax",**self.kwargs)
+    if self.add_dropout:
+      self.drop_expert_layer = ExpertUtilizationDropout(self.dropout_rate)
+    super().build(batch_input_shape) 
+      
+  def call(self, 
+           inputs: Union[np.array, tf.Tensor],
+           training: bool
+          ) -> tf.Tensor:
+    """
+    Defines set of computations performed in the layer
+
+    Parameters
+    ----------
+    inputs: array-like structure 
+      Inputs to the layer (can be numpy array, tf.Tensor, list/tuple of numpy 
+      array or tensorflow Tensors)
+
+    training: bool
+      True if layer is called in training mode, False otherwise 
+
+    Returns
+    -------
+    moe_output: tf.Tensor
+      Output of mixture of experts layers ( linearly weighted output of expert
+      layers).
+    """
+    # compute each expert output
+    experts_output = [expert(inputs) for expert in self.expert_layers]
+
+    # compute probability of expert (degree of expert utilization) for given
+    # input set
+    expert_utilization_prob = self.expert_probs(inputs)
+    if self.add_dropout:
+      expert_utilization_prob = self.drop_expert_layer(expert_utilization_prob,
+                                                       training)
+      
+    # compute weighted output of experts 
+    moe_output = 0
+    for i, expert_output in enumerate(experts_output):
+      moe_output += (expert_output
+                     *tf.expand_dims(expert_utilization_prob[:,i], axis=-1)
+                    )
+    return moe_output
+
+  def get_config(self) -> Dict:
+    """ Config of the MOE layer """
+    base_config = super().get_config()
+    return {**base_config,
+            "add_dropout": self.add_dropout,
+            "dropout_rate": self.dropout_rate,
+            "expert_layers": [tf.keras.layers.serialize(layer) for 
+                              layer in self.expert_layers]
+            }
---
+++ utils/load_census_data.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+from typing import Tuple, List
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+UCI_CENSUS_TRAIN_DATA = "https://archive.ics.uci.edu/ml/machine-learning-databases/census-income-mld/census-income.data.gz"
+UCI_CENSUS_TEST_DATA = "https://archive.ics.uci.edu/ml/machine-learning-databases/census-income-mld/census-income.test.gz"
+
+COLUMNS = ['age',
+           'class_worker',
+           'det_ind_code',
+           'det_occ_code',
+           'education', 
+           'wage_per_hour',
+           'hs_college',
+           'marital_stat',
+           'major_ind_code', 
+           'major_occ_code',
+           'race',
+           'hisp_origin',
+           'sex',
+           'union_member',
+           'unemp_reason',
+           'full_or_part_emp',
+           'capital_gains', 
+           'capital_losses',
+           'stock_dividends',
+           'tax_filer_stat', 
+           'region_prev_res',
+           'state_prev_res',
+           'det_hh_fam_stat',
+           'det_hh_summ',
+           'instance_weight',
+           'mig_chg_msa',
+           'mig_chg_reg',
+           'mig_move_reg',
+           'mig_same',
+           'mig_prev_sunbelt',
+           'num_emp', 
+           'fam_under_18',
+           'country_father', 
+           'country_mother', 
+           'country_self',
+           'citizenship',
+           'own_or_self', 
+           'vet_question', 
+           'vet_benefits',
+           'weeks_worked', 
+           'year',
+           'income_50k']
+
+
+def load_census_data_uci(local_path: str=None,
+                         uci_train_link_path: str=UCI_CENSUS_TRAIN_DATA,
+                         uci_test_link_path: str=UCI_CENSUS_TEST_DATA,
+                         write_data_locally: bool=True,
+                         columns: List[str]=COLUMNS
+                         ) -> Tuple[pd.DataFrame]:
+  """ 
+  Checks whether data are in path directory or in current directory if path is 
+  not specified, loads data from the uci and splits test set into 
+  new test and validation sets (in the proportion 1:1) as described in the 
+  paper. 
+  
+  Parameters
+  ----------
+  local_path: str, optional (default=None)
+    Local path where all the data are stored, if not provided current directory
+    will be used.
+
+  uci_train_link_path: str, optional (default=UCI_CENSUS_TRAIN_DATA)
+    Path to training data (in UCI Machine Learning Datasets)
+
+  uci_test_link_path: str, optional (default=UCI_CENSUS_TEST_DATA)
+    Path to original test data (in UCI Machine Learning Datasets). Note that 
+    in the paper original test data are split into validation and test 
+    with ratio 1:1.
+
+  write_data_locally: bool, optional (default=True)
+    If True will write train and original test datasets to local path if they 
+    are not there already.
+
+  columns: list of str
+    Names of the columns in dataset
+
+  Returns
+  -------
+  : tuple of pandas.DataFrame 
+    Tuple of data frames (train, validation, test)
+
+  References
+  ----------
+  [1] Modeling Task Relationships in Multi-task Learning with 
+      Multi-gate Mixture-of-Experts, Jiaqi Ma1 et al, 2018 KDD
+  """
+  train_file, test_file = "census-income.data", "census-income.test"
+  if not local_path:
+    local_path = os.getcwd()
+  train_file_path = os.path.join(local_path, train_file)
+  test_file_path = os.path.join(local_path, test_file)
+  
+  # load training data 
+  if train_file in os.listdir(local_path):
+    data_train = pd.read_csv(train_file_path, 
+                             index_col=0)
+  else:
+    data_train = pd.read_csv(uci_train_link_path, 
+                             header=None, 
+                             index_col=False, 
+                             names=COLUMNS)
+    
+  # load test data 
+  if test_file in os.listdir(local_path):
+     data_test_orig = pd.read_csv(test_file_path, 
+                                  index_col=0)
+  else:
+     data_test_orig = pd.read_csv(uci_test_link_path, 
+                                  header=None, 
+                                  index_col=False, 
+                                  names=COLUMNS)
+
+  # write loaded data to local path if they are not there already
+  if write_data_locally:
+    if train_file not in os.listdir(local_path):
+      data_train.to_csv(train_file_path)
+    if test_file not in os.listdir(local_path):
+      data_test_orig.to_csv(test_file_path)
+
+  # drop instance weight ( in section 6.3 of the paper there is no mention
+  # of using instance_weight as sample weights for the loss functions)
+  data_train.drop("instance_weight", axis=1, inplace=True)
+  data_test_orig.drop("instance_weight", axis=1, inplace=True)
+
+  # split original test data into training and validation as mentioned 
+  # in the paper
+  data_val, data_test = train_test_split(data_test_orig, test_size=0.5)
+  return data_train, data_val, data_test
