a1e8d82925640db291debf7483250eb52228ffdd
AmazaspShumik <amazasp.shaumyan@gmail.com>
2020-01-12 12:06:46 -0800

    adding tuner to the package

--- mtlearn/experiments/uci_income_census/__init__.py
+++ mtlearn/experiments/uci_income_census/__init__.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 from . import utils
 from .uci_census_preprocessing_layer import PreprocessingLayer
+from .tuner import UCICensusIncomeTuner
 from .hyper_models import (build_hyper_l2_constrained,
                            build_hyper_mmoe_model,
                            build_hyper_omoe_model,
--- mtlearn/experiments/uci_income_census/tuner.py
+++ mtlearn/experiments/uci_income_census/tuner.py
@@ -4,7 +4,7 @@ from sklearn.metrics import roc_auc_score
 from tensorflow.keras.optimizers import Adam, SGD, RMSprop
 
 
-class IncomeCensusTuner(kt.Tuner):
+class UCICensusIncomeTuner(kt.Tuner):
     """
     Hyperparameter tuner for Multi-task models on Income Census dataset
     """
